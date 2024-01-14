3b03f14f23e4835691c53300b9751611fc846618
james <james@bubbleye.com>
2024-01-14 16:28:43 +0800

    Use arraymesh instead of gridmap

---
+++ MeshGenWater.gd
@@ -0,0 +1,32 @@
+@tool
+extends MeshInstance3D
+
+@export var update = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	gen_water()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if update:
+		gen_water()
+		
+func gen_water():
+	var a_mesh = ArrayMesh.new()
+	var vertices := PackedVector3Array([
+		Vector3(-100,-10,-100),
+		Vector3(100,-10,-100),
+		Vector3(100,-10,100),
+		Vector3(-100,-10,100),
+	])
+	var indices := PackedInt32Array([
+		0,1,2,
+		0,2,3
+	])
+	var array = []
+	array.resize(Mesh.ARRAY_MAX)
+	array[Mesh.ARRAY_VERTEX] = vertices
+	array[Mesh.ARRAY_INDEX] = indices
+	a_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES,array)
+	mesh = a_mesh
--- ball.gd
+++ ball.gd
@@ -1,34 +1,56 @@
 extends RigidBody3D
 
-const acceleration = 1.0001
+const acceleration = 1.01
 var motion = Vector3()
-const max_speed = -10
-const JUMP_FORCE = Vector3(0, 40, 0)  # Adjust the Y value to set jump strength
+const max_force = -100
+const min_speed = -20
+const force_side = 10
+const JUMP_FORCE = 500
 
 var is_on_ground = false  # Variable to check if the player is on the ground
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass  # Replace with function body.
+	set_contact_monitor(true)
+	set_max_contacts_reported (2)
+	connect("body_entered", _on_body_entered)
+	 
 
+func _on_body_exited(body:Node):
+	print(body, " exited")
+	
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	motion.x = Input.get_axis("move_left", "move_right") * 4
-	if Input.is_action_pressed("move_forward"):
-		motion.z = motion.z + Input.get_axis("move_forward", "move_back") * acceleration
-		motion.z = max(max_speed, motion.z)
+	var c = get_contact_count()
+	if c > 0:
+		is_on_ground = true
 	else:
-		motion.z = motion.z * 0.9
+		is_on_ground = false
+	motion.x = Input.get_axis("move_left", "move_right") * force_side
+	var speed = min(min_speed, motion.z*(1+c))
+	if Input.is_action_pressed("move_forward") and not is_on_ground:
+		speed = speed * acceleration
+		speed = max(max_force, speed)
+	else:
+		speed = min_speed
+	motion.z = speed
+	var y_speed = motion.y
 	# Check for jump input
+	if not is_on_ground:
+		y_speed = -max(abs(y_speed),1) * 11 * delta
 	if (Input.is_action_just_pressed("jump") and is_on_ground):
 		print("jump!")
-		apply_central_impulse(JUMP_FORCE)
-		motion.y += JUMP_FORCE
+		#apply_central_impulse(JUMP_FORCE)
+		y_speed = y_speed + JUMP_FORCE * 10
 		is_on_ground = false  # Player has jumped, so they're no longer on the ground
-	apply_central_force(motion * 1200.0 * delta)
+	motion.y = y_speed
+	print(motion)
+	apply_central_force(motion *2* delta)
 
 
-	if global_transform.origin.y < -10:
+	if global_transform.origin.y < -49:
 		game_over()
 
 func game_over():
@@ -38,10 +60,15 @@ func game_over():
 
 # Override the _integrate_forces function to detect if the player is on the ground
 func _integrate_forces(state):
-	var local_floor_normal = Vector3(0, 1, 0)
 	is_on_ground = state.get_contact_count() > 0
-	print(is_on_ground)
 	#for i in range(state.get_contact_count()):
 		#if state.get_contact_local_normal(i).dot(local_floor_normal) > 0.9:
 			#is_on_ground = true
 			#break
+			
+
+# Callback function for the body_entered signal
+func _on_body_entered(body):
+	print("RigidBody3D is touching the ...")
+	if body is GridMap:
+		print("RigidBody3D is touching the GridMap")
