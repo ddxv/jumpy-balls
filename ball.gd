8f987a8740681a88fd39c9f3ec3bd2c488f60fca
james <james@bubbleye.com>
2024-01-15 11:40:06 +0800

    Try using angular momentum, jump not working

--- ball.gd
+++ ball.gd
@@ -8,6 +8,13 @@ const force_side = min_speed
 const JUMP_FORCE = -max_force * 2 
 var last_contact_count =0;
 
+var CameraRig;
+var FloorCheck;
+
+
+
+
+
 var is_on_ground = false  # Variable to check if the player is on the ground
 
 
@@ -17,53 +24,92 @@ func _ready():
 	set_contact_monitor(true)
 	set_max_contacts_reported (2)
 	#connect("body_entered", _on_body_entered)
+	var parent = get_parent()
+	CameraRig = parent.get_node("CameraRig")
+	CameraRig.top_level =true
+	FloorCheck = parent.get_node("FloorCheck")
+	FloorCheck.top_level = true
+	# Make sure FloorCheck raycast doesn't inherit Balls transforms,
+	# so that it doesn't rotate as the ball rolls
+	#$FloorCheck.set_as_toplevel(true)
+	
+	
 	 
 
-func _on_body_exited(body:Node):
-	print(body, " exited")
-	
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var y_speed = 0
-	var c = get_contact_count()
-	if c > 0:
-		is_on_ground = true
-		print('on ground')
-	elif last_contact_count ==0 and c==0:
-		print('off ground')
-		is_on_ground = false
-		y_speed = motion.y
-	last_contact_count = c
-	motion.x = Input.get_axis("move_left", "move_right") * force_side
-	var speed = min(min_speed, motion.z*(1+c))
-	if Input.is_action_pressed("move_forward") and not is_on_ground:
-		speed = speed * acceleration
-		speed = max(max_force, speed)
-	else:
-		speed = min_speed
-	motion.z = speed
-
-	# Check for jump input
-	if not is_on_ground:
-		print("add falling speed")
-		y_speed = min(abs(y_speed),-1) * 1200 * delta
-	if (Input.is_action_just_pressed("jump") and is_on_ground):
-		print("jump!")
-		y_speed = abs(speed) + y_speed + JUMP_FORCE * 10
-		apply_central_impulse(Vector3(0,JUMP_FORCE,0))
-	motion.y = y_speed
-	print(motion)
-	apply_central_force(motion *2* delta)
-
-
-	if global_transform.origin.y < -49:
-		game_over()
+#func _on_body_exited(body:Node):
+	#print(body, " exited")
+	#
+	#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _physics_process(delta):
+	#var y_speed = 0
+	#var c = get_contact_count()
+	#if c > 0:
+		#is_on_ground = true
+		#print('on ground')
+	#elif last_contact_count ==0 and c==0:
+		#print('off ground')
+		#is_on_ground = false
+		#y_speed = motion.y
+	#last_contact_count = c
+	#motion.x = Input.get_axis("move_left", "move_right") * force_side
+	#var speed = min(min_speed, motion.z*(1+c))
+	#if Input.is_action_pressed("move_forward") and not is_on_ground:
+		#speed = speed * acceleration
+		#speed = max(max_force, speed)
+	#else:
+		#speed = min_speed
+	#motion.z = speed
+#
+	## Check for jump input
+	#if not is_on_ground:
+		#print("add falling speed")
+		#y_speed = min(abs(y_speed),-1) * 1200 * delta
+	#if (Input.is_action_just_pressed("jump") and is_on_ground):
+		#print("jump!")
+		#y_speed = abs(speed) + y_speed + JUMP_FORCE * 10
+		#apply_central_impulse(Vector3(0,JUMP_FORCE,0))
+	#motion.y = y_speed
+	#print(motion)
+	#apply_central_force(motion *2* delta)
+#
+	#if global_transform.origin.y < -49:
+		#game_over()
 
 func game_over():
 	# Handle the game over state
 	print("Game Over!")  # Replace this with your game over logic
 	get_tree().reload_current_scene()
+
+
+var rolling_force = 40
+
+
+func _physics_process(delta):
+	CameraRig.global_transform.origin = lerp(
+		CameraRig.global_transform.origin, 
+		global_transform.origin, 0.1
+	)
+	# As the ball moves, move the raycast along with it
+	FloorCheck.global_transform.origin = global_transform.origin
+
+	if Input.is_action_pressed("move_forward"):
+		angular_velocity.x -= rolling_force*delta
+	elif Input.is_action_pressed("move_back"):
+		angular_velocity.x += rolling_force*delta
+	if Input.is_action_pressed("move_left"):
+		angular_velocity.z += rolling_force*delta
+	elif Input.is_action_pressed("move_right"):
+		angular_velocity.z -= rolling_force*delta
+
+	# When the ball is on the floor and the user presses jump button,
+	# add impulse moving the ball up.
+	if Input.is_action_just_pressed("jump") and FloorCheck.is_colliding():
+		print("jump")
+		apply_impulse(Vector3(), Vector3.UP*10000)
+
+
+
 
 ## Override the _integrate_forces function to detect if the player is on the ground
 #func _integrate_forces(state):
