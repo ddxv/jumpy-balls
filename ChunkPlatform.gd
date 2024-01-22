76ec46289db00d878a90d1c15b4bccac3bafda69
james <james@bubbleye.com>
2024-01-22 13:13:34 +0800

    Less ramps and terrain to sides, generate ramps less often

--- ChunkPlatform.gd
+++ ChunkPlatform.gd
@@ -2,13 +2,12 @@ class_name ChunkPlatform
 extends MeshInstance3D
 
 const CENTER_OFFSET = 0.5
-const RAMP_HEIGHT = 200
 
-#Terrain size
-@export_range(20, 400, 1) var terrain_size := 200
+# Terrain size
+# @export_range(20, 400, 1) var terrain_size := 200
 #LOD scaling
 @export_range(1, 100, 1) var resolution := 20
-@export var terrain_max_height = 5
+# @export var terrain_max_height = 5
 
 @export var chunk_lods: Array[int] = [2, 4, 8, 15, 20, 50]
 @export var lod_distances: Array[int] = [2000, 1500, 1050, 900, 790, 550]
@@ -17,7 +16,7 @@ const RAMP_HEIGHT = 200
 var position_coord = Vector2()
 var grid_coord = Vector2()
 
-var set_collision = false
+var set_collision = true
 
 var my_multiplier = 0
 
@@ -27,7 +26,7 @@ func gen_platform(surftool: SurfaceTool, size: int, my_height: int, is_ramp: boo
 	var my_ramp_height = 0
 
 	if is_ramp:
-		my_ramp_height = RAMP_HEIGHT
+		my_ramp_height = Globals.RAMP_HEIGHT
 
 	var vertices := PackedVector3Array(
 		[
@@ -79,9 +78,9 @@ func generate_platforms(multiplier: int, coords: Vector2, size: int, initailly_v
 		my_multiplier = multiplier
 	#set 2D position in world space
 	grid_coord = coords
-	var my_height = my_multiplier * RAMP_HEIGHT
-	var my_height_2f = (my_multiplier * RAMP_HEIGHT) + RAMP_HEIGHT
-	var my_height_3f = (my_multiplier * RAMP_HEIGHT) + RAMP_HEIGHT * 2
+	var my_height = my_multiplier * Globals.RAMP_HEIGHT
+	var my_height_2f = (my_multiplier * Globals.RAMP_HEIGHT) + Globals.RAMP_HEIGHT
+	var my_height_3f = (my_multiplier * Globals.RAMP_HEIGHT) + Globals.RAMP_HEIGHT * 2
 	var grid_coord_south = grid_coord + Vector2(0, 1)
 	var grid_coord_south_south = grid_coord + Vector2(0, 2)
 	# var grid_coord_south_south_south = grid_coord + Vector2(0, 3)
